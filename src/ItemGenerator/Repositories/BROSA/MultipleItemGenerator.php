b598dd54358a1a3d4698a714d9b95cc5b7558903
Ivan Li <ivan.li@INVIGOR.local>
2017-07-13 17:14:01 +1000

    added brosa

---
+++ src/ItemGenerator/Repositories/BROSA/MultipleItemGenerator.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ivan.li
+ * Date: 13/07/2017
+ * Time: 5:04 PM
+ */
+
+namespace IvanCLI\ItemGenerator\Repositories\BROSA;
+
+
+use IvanCLI\ItemGenerator\Contracts\ItemGenerator;
+
+class MultipleItemGenerator extends ItemGenerator
+{
+    const OPTION_TYPE_1_REGEX = '#var variant_option_details = (.*?)var #si';
+
+    protected $content;
+    protected $items;
+    protected $options;
+
+    protected $productOptions;
+
+    /**
+     * Set content, HTML most of the time
+     * @param $content
+     * @return mixed
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * check if content has multiple items
+     * @return bool
+     */
+    public function hasMultipleItems()
+    {
+        $this->__getProductOptions();
+        if (!is_null($this->productOptions) && count($this->productOptions) > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * collect label and options from content
+     * @return mixed
+     */
+    public function extractOptions()
+    {
+        if ($this->hasMultipleItems()) {
+            $items = [];
+            foreach ($this->productOptions as $productOption) {
+                $id = $productOption->sku;
+                $text = $productOption->sku;
+
+                $item = [];
+                $item["Variation"] = new \stdClass();
+                $item["Variation"]->text = $text;
+                $item["Variation"]->value = $id;
+                $items[] = $item;
+            }
+            $this->options = $items;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * returning structural options
+     * @return mixed
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    /**
+     * returning a list of items need to be generated
+     * @return mixed
+     */
+    public function getItems()
+    {
+        return $this->items;
+    }
+
+    /**
+     * combine multiple arrays
+     * @param array $data
+     * @param array $all
+     * @param array $group
+     * @param null $value
+     * @param int $i
+     * @return array
+     */
+    public function combinations(array $data, array &$all = array(), array $group = array(), $value = null, $i = 0)
+    {
+        $this->items = $this->options;
+        return $this->items;
+    }
+
+    private function __getProductOptions()
+    {
+
+        if (!is_null($this->content)) {
+            preg_match(self::OPTION_TYPE_1_REGEX, $this->content, $matches);
+            if (isset($matches[1])) {
+                $matchOptions = $matches[1];
+                $matchOptions = trim($matchOptions);
+            }
+            if (isset($matchOptions) && !is_null($matchOptions)) {
+                $productOptions = json_decode($matchOptions);
+                if (!is_null($productOptions) && json_last_error() === JSON_ERROR_NONE) {
+                    $this->productOptions = $productOptions;
+                    return true;
+                }
+            }
+        }
+    }
+}
